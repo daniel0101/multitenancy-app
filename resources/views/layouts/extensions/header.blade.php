e5371e0adf7a234a1bfa2c0d0656f36ebfe3c0b6
awujoola <danielawujoola1@gmail.com>
2023-08-03 13:14:12 +0100

    frontend presets

---
+++ resources/views/layouts/extensions/header.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" class="h-full bg-[color:var(--html-bg-color,theme(colors.white))]">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <link rel="icon" href="/favicon.ico">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+
+        <link rel="preload" href="/fonts/Inter-roman.var.woff2?v=3.18" as="font" type="font/woff2" crossorigin="">
+        <script src="https://cdn.usefathom.com/script.js" data-site="SOXJJCZQ" data-auto="false" defer></script>
+        <script src="https://cdn.paddle.com/paddle/paddle.js"></script>
+        <link rel="preload" as="style" href="https://tailwindui.com/build/assets/app-fb087be6.css" />
+        <link rel="modulepreload" href="https://tailwindui.com/build/assets/app-e0f43721.js" />
+        <link rel="modulepreload" href="https://tailwindui.com/build/assets/index-0c85f979.js" />
+        <link rel="modulepreload" href="https://tailwindui.com/build/assets/_commonjsHelpers-87174ba5.js" />
+        <link rel="stylesheet" href="https://tailwindui.com/build/assets/app-fb087be6.css" />
+        <script type="module" src="https://tailwindui.com/build/assets/app-e0f43721.js"></script>    
+        <meta name="twitter:card" content="summary_large_image" inertia>
+        <meta name="twitter:site" content="@tailwindcss" inertia>
+        <meta name="twitter:title" content="Hero Sections - Official Tailwind CSS UI Components" inertia>
+        <meta name="twitter:description" content="Hero section examples for Tailwind CSS, designed and built by the creators of the framework." inertia>
+        <meta name="twitter:image" content="https://tailwindui.com/img/og-default.png" inertia>
+        <meta name="twitter:creator" content="@tailwindcss" inertia>
+        <meta property="og:url" content="https://www.tailwindui.com/" inertia>
+        <meta property="og:type" content="article" inertia>
+        <meta property="og:title" content="Hero Sections - Official Tailwind CSS UI Components" inertia>
+        <meta property="og:description" content="Hero section examples for Tailwind CSS, designed and built by the creators of the framework." inertia>
+        <meta property="og:image" content="https://tailwindui.com/img/og-default.png" inertia>
+        <meta property="description" content="Hero section examples for Tailwind CSS, designed and built by the creators of the framework." inertia>
+        <title inertia>Multi Tenancy App</title> 
+    </head>
