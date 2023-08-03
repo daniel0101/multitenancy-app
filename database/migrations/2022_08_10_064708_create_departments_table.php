f710159fc34de6ca5df9a5df61b2402691d28c24
awujoola <danielawujoola1@gmail.com>
2023-08-03 13:15:28 +0100

    database migrations

--- database/migrations/2022_08_10_064708_create_departments_table.php
+++ database/migrations/2022_08_10_064708_create_departments_table.php
@@ -13,16 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('departments', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('tenant_id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('role')->nullable();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
-            $table->unsignedBigInteger('tenant_id')->nullable();
         });
     }
 
@@ -33,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('departments');
     }
 };
